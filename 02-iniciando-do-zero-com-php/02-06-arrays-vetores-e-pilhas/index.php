836971a4e1c7c4ee1037c278f0f7a2362eb02fcd
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2023-07-22 19:04:27 -0300

    array vetores

--- 02-iniciando-do-zero-com-php/02-06-arrays-vetores-e-pilhas/index.php
+++ 02-iniciando-do-zero-com-php/02-06-arrays-vetores-e-pilhas/index.php
@@ -7,18 +7,55 @@ fullStackPHPClassName("02.06 - Arrays, vetores e pilhas");
  */
 fullStackPHPClassSession("index array", __LINE__);
 
+$arrA = array(1, 2, 3);
+$arrA = [0,1,2,3];
+
+var_dump($arrA);
+
+$arrayIndex = [
+    "Brian",
+    "Angus",
+    "Malcolm",
+];
+
+$arrayIndex[] = "Cliff";
+$arrayIndex[] = "Phil";
+
+var_dump($arrayIndex);
 
 /**
  * [ associative array ] "key" => "value"
  */
 fullStackPHPClassSession("associative array", __LINE__);
 
+$arrayAssoc = [
+    "vocal" => "Brian",
+    "solo_guitar" => "Angus",
+    "base_guitar" => "Malcolm",
+    "bass_guitar" => "Cliff",
+];
+$arrayAssoc["drums"] ="Phil";
+$arrayAssoc["rock_band"] ="AC/DC";
+
+var_dump($arrayAssoc);
 
 /**
  * [ multidimensional array ] "key" => ["key" => "value"]
  */
 fullStackPHPClassSession("multidimensional array", __LINE__);
 
+$brian = ["Brian", "Mic"];
+$angus = ["name" => "Angus", "intrument" => "Guitar"];
+$instruments = [
+    $brian,
+    $angus
+];
+var_dump($instruments);
+
+var_dump([
+    "brian" => $brian,
+    "angus" => $angus
+]);
 
 /**
  * [ array access ] foreach(array as item) || foreach(array as key => value)
