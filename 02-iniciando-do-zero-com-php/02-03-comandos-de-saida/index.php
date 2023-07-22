2fb60a59b79f49a3908d190a1e7066e93bedb537
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2023-07-22 14:55:46 -0300

    Comandos de saida

--- 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
+++ 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
@@ -7,11 +7,35 @@ fullStackPHPClassName("02.03 - Comandos de saída");
  */
 fullStackPHPClassSession("echo", __LINE__);
 
+echo "<p>Olá mundo", " ", "<span class='tag'>#BoraProgramar!</span>", "</p>";
 
+$hello = "Olá mundo!";
+$code = "<span class='tag'>#BoraProgramar!</span>";
+
+echo "<p>$hello</p>";
+echo '<p>$hello</p>';
+
+$day = "dia";
+echo "<p>Falta 1 $day para o evento !</p>";
+echo "<p>Faltam 2 {$day}s para o evento !</p>";
+
+echo"<h3>{$hello}</h3>";
+echo"<h4>{$hello} {$code}</h4>";
+
+echo '<h3>' .$hello. " " .$code."</h3>";
+?>
+
+<h4><?= $hello; ?> <?= $code; ?></h4>
+
+<?php
 /**
  * [ print ] https://php.net/manual/pt_BR/function.print.php
  */
 fullStackPHPClassSession("print", __LINE__);
+
+print $hello;
+print $code;
+print "<h3>{$hello} {$code}</h3>";
 
 
 /**
@@ -19,11 +43,32 @@ fullStackPHPClassSession("print", __LINE__);
  */
 fullStackPHPClassSession("print_r", __LINE__);
 
+$array = [
+    "company" => "UpInside",
+    "course" => "FSPHP",
+    "class" => "Comandos de saída",
+];
+
+print_r($array);
+echo "<pre>", print_r($array, true),"</pre>";
 
 /**
  * [ printf ] https://php.net/manual/pt_BR/function.printf.php
  */
 fullStackPHPClassSession("printf", __LINE__);
+
+$article = "<article><h1>%s</h1><p>%s</p></article>";
+$title = "{$hello} {$code}";
+$subtitle = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essentially unchanged. It was popularised in the 1960s with the release of 
+    Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+    software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+printf($article, $title, $subtitle);
+echo printf($article, $title, $subtitle);
 
 
 /**
@@ -31,8 +76,13 @@ fullStackPHPClassSession("printf", __LINE__);
  */
 fullStackPHPClassSession("vprintf", __LINE__);
 
+$company = "<article><h1>Escola %s</h1><p>Curso <b>%s</b>,aula <b>%s</b></p></article>";
+vprintf($company, $array);
+echo vprintf($company, $array);
 
 /**
  * [ var_dump ] https://php.net/manual/pt_BR/function.var-dump.php
  */
 fullStackPHPClassSession("var_dump", __LINE__);
+
+var_dump($array,$hello,$code);
