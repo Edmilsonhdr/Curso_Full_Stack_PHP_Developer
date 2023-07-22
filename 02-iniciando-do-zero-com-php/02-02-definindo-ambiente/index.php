b8c8aa5cb6a9949af9e0da9d00be9a4353cbaba1
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2023-07-22 14:09:30 -0300

    Definindo ambiente

---
+++ 02-iniciando-do-zero-com-php/02-02-definindo-ambiente/index.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__ . '/../../fullstackphp/fsphp.php';
+fullStackPHPClassName("02.02 - Definindo ambiente");
+
+/*
+ * Documentando exemplo do codigo
+ */
+fullStackPHPClassSession("Debug Section", __LINE__);
